6e3ed6c89e5f67f23492b37fb6e657614a473965
Ilan221 <ilantr1203@gmail.com>
2017-10-10 18:28:35 +0300

    third commit

--- Ilan_GateWay.py
+++ Ilan_GateWay.py
@@ -1,11 +1,47 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask import json
+from flask import Response
+from flask import request
+import paho.mqtt.client as mqtt
+import os, urlparse
+
 app = Flask(__name__)
+@app.route("/", methods =["GET", "POST"])
+def Welcome():
+    return "Welcome to my page :)"
 
-@app.route("/", methods=["GET", "POST"])
-def Ilan_GateWay():
-    data = request.get_json()
-    param = data["param"]
-    return jsonify({"result" : "Success", "param" : param})
+@app.route('/articles')
+def api_articles():
+    return 'List of articles'
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+@app.route('/json', methods = ['GET', 'POST'])
+def api_json():
+    data = {k:v for k,v in request.args.items()}
+    return json.dumps(data)
+
+@app.route('/hello', methods = ['GET', 'POST'])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    return json.dumps(data)
+
+server = server()
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0' port = int(os.environ.get("POST", 5000)))
+#from flask import Flask, jsonify, request
+#app = Flask(__name__)
+
+#@app.route("/", methods=["GET", "POST"])
+#def Ilan_GateWay():
+ #   data = request.get_json()
+  #3  param = data["param"]
+    #return jsonify({"result" : "Success", "param" : param})
+
+#if __name__ == "__main__":
+ #   app.run()
