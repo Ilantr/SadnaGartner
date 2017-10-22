8a99e5bdcc3843d294d0a1c6ab8fc722ff614e46
Ilan221 <ilantr1203@gmail.com>
2017-10-22 19:54:07 +0300

    Sixth

--- Ilan_GateWay.py
+++ Ilan_GateWay.py
@@ -2,35 +2,17 @@ from flask import Flask
 from flask import json
 from flask import Response
 from flask import request
-import paho.mqtt.client as mqtt
-import os, urlparse
+import os
 
-app = Flask(__name__)
-@app.route("/", methods =["GET", "POST"])
-def Welcome():
-    return "Welcome to my page :)"
+class Server(object):
+    """Server class"""
+    app = Flask(__name__)
 
-@app.route('/articles')
-def api_articles():
-    return 'List of articles'
+    @app.route("/", methods =['GET', 'POST'])
+    def Welcome():
+        return "Welcome to my page :)"
 
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
-
-@app.route('/json', methods = ['GET', 'POST'])
-def api_json():
-    data = {k:v for k,v in request.args.items()}
-    return json.dumps(data)
-
-@app.route('/hello', methods = ['GET', 'POST'])
-def api_hello():
-    data = {
-        'hello'  : 'world',
-        'number' : 3
-    }
-    return json.dumps(data)
-
-server = server()
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = int(os.environ.get("POST", 5000)))
+    if __name__ == '__main__':
+        app.debug = True
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host = '0.0.0.0', port = port)
