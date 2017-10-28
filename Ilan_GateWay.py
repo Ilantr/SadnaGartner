ab1cc50179310b152922f2c0cad627d6af363636
Ilan221 <ilantr1203@gmail.com>
2017-10-28 18:00:48 +0300

    b

--- Ilan_GateWay.py
+++ Ilan_GateWay.py
@@ -14,29 +14,8 @@ class Server(object):
     MQTTC = MqttClient()
 
     @app.route("/")
-    def Welcome():
-        return "Welcome to my page :)"
-
-    @app.route('/articles')
-    def api_articles():
-        return 'List of articles'
-
-    @app.route('/articles/<articleid>')
-    def api_article(articleid):
-        return 'You are reading ' + articleid
-
-    @app.route('/json', methods = ['GET', 'POST'])
-    def api_json():
-        data = {k:v for k,v in request.args.items()}
-        return json.dumps(data)
-
-    @app.route('/hello', methods = ['GET', 'POST'])
-    def api_hello():
-        data = {
-            'hello'  : 'world',
-            'number' : 3
-        }
-        return json.dumps(data)
+    def getRequest():
+        return "Hello World!"
 
     """webhook api"""
     @app.route("/webhook", methods=['GET'])
